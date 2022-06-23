a0d9016262ae4faba0dd891149bb1e80de46290a
matheusncodello <matheus.ncodello@gmail.com>
2022-06-23 10:02:11 -0300

    build: first project build

---
+++ src/main/kotlin/com/matheusncodello/webflux/SpringWebfluxCrudApplication.kt
@@ -0,0 +1,11 @@
+package com.matheusncodello.webflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxCrudApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebfluxCrudApplication>(*args)
+}
---
+++ src/test/kotlin/com/webflux/SpringWebfluxCrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.matheusncodello.webflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebfluxCrudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
